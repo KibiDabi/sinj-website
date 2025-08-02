a34043673ea9bd3a7a13c3ab8022932fa31e62c8
KibiDabi <s_lj_91@yahoo.com>
2025-08-02 05:03:49 +0200

    'Adding Suspense wrapper'

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,14 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import ContentSwitcher from "../components/content-switcher";
-import { use } from "react";
+import { Suspense, use } from "react";
 
-export default function HomePage({ params }: { params: Promise<{ locale: string }> }) {
-
-    const { locale } = use(params);
+export default function HomePage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = use(params);
 
   return (
     <SidebarProvider
@@ -16,16 +19,21 @@ export default function HomePage({ params }: { params: Promise<{ locale: string 
       }
     >
       <div className="flex h-full w-full">
-      <SidebarInset className="bg-transparent">
+        <SidebarInset className="bg-transparent">
           <div
             className="flex  w-full pt-10 2xl:pt-18 "
             style={{ height: "calc(100vh - 56px)" }}
           >
-            <AppSidebar locale={locale} className="h-full" style={{ position: "relative"}} />
-            <ContentSwitcher locale={locale} />
+            <AppSidebar
+              locale={locale}
+              className="h-full"
+              style={{ position: "relative" }}
+            />
+            <Suspense>
+              <ContentSwitcher locale={locale} />
+            </Suspense>
           </div>
-          
-      </SidebarInset>
+        </SidebarInset>
       </div>
     </SidebarProvider>
   );
